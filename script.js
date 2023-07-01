ac1e91362a11db0a03a27396852b7e9784c43ec0
kabornblack <shaibu.kabiru@email.com>
2023-07-01 10:48:47 +0300

    Final code

--- script.js
+++ script.js
@@ -5,18 +5,10 @@ btn.addEventListener("click", palindrome);
 
 function palindrome() {
     const word = document.querySelector(".input-text").value;
-    let len = word.length;
 
-    let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
-    let end = word.substring(len, Math.floor(len / 2)).toLowerCase();
-
-    let flip = end.split("").reverse().join("");
-
-    if (start === flip) {
+    if (word === word.split("").reverse().join("")) {
         result.innerHTML = `${word.toUpperCase()} is a Palindrome`;
     } else {
         result.innerHTML = `${word.toUpperCase()} is not a Palindrome`;
     }
-
-    //alert(end);
 }
